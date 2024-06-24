0845b1308dd005432ebd7224cdfcb65d38652a70
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-06-24 23:28:52 +0400

    Install react scroll, add some file structure

---
+++ src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavigationPage from "../navigationPage/NavigationPage";
+import FooterPage from "../footerPage/FooterPage";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <NavigationPage />
+      <FooterPage />
+    </div>
+  );
+};
+
+export default LandingPage;
