99a596bf38b7bd2960e0f45744dc825a94a646ef
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-06-25 22:05:23 +0400

    Add client logos

--- src/components/clientsComponents/infiniteScroll/InfiniteScroll.jsx
+++ src/components/clientsComponents/infiniteScroll/InfiniteScroll.jsx
@@ -1,13 +1,42 @@
 import React from "react";
-import { ClientLogo, Container, ScrollContainer } from "./style";
+import {
+  ClientLogo,
+  Container,
+  ScrollContainer1,
+  ScrollContainer2,
+} from "./style";
 import WB from "../../../assets/images/clientLogos/client-01.png";
+import Netflix from "../../../assets/images/clientLogos/client-02.png";
+import Aviacom from "../../../assets/images/clientLogos/client-03.png";
+import Neon from "../../../assets/images/clientLogos/client-04.png";
+import AppleTV from "../../../assets/images/clientLogos/client-05.png";
+import A24 from "../../../assets/images/clientLogos/client-06.png";
+import Universal from "../../../assets/images/clientLogos/client-07.png";
+import Sony from "../../../assets/images/clientLogos/client-08.png";
+import PrimeVideo from "../../../assets/images/clientLogos/client-09.png";
+import HBO from "../../../assets/images/clientLogos/client-10.png";
+import Hulu from "../../../assets/images/clientLogos/client-11.png";
+import Ubisoft from "../../../assets/images/clientLogos/client-12.png";
 
 const InfiniteScroll = () => {
   return (
     <Container>
-      <ScrollContainer>
+      <ScrollContainer1>
         <ClientLogo src={WB} />
-      </ScrollContainer>
+        <ClientLogo src={Netflix} />
+        <ClientLogo src={Aviacom} />
+        <ClientLogo src={Neon} />
+        <ClientLogo src={AppleTV} />
+        <ClientLogo src={A24} />
+      </ScrollContainer1>
+      <ScrollContainer2>
+        <ClientLogo src={Universal} />
+        <ClientLogo src={Sony} />
+        <ClientLogo src={PrimeVideo} />
+        <ClientLogo src={HBO} />
+        <ClientLogo src={Hulu} />
+        <ClientLogo src={Ubisoft} />
+      </ScrollContainer2>
     </Container>
   );
 };
--- src/components/clientsComponents/infiniteScroll/style.js
+++ src/components/clientsComponents/infiniteScroll/style.js
@@ -2,11 +2,20 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
-export const ScrollContainer = styled.div`
+export const ScrollContainer1 = styled.div`
   display: flex;
-  justify-content: flex-start;
+  flex-direction: row;
+  gap: 150px;
+  margin-bottom: 70px;
+  padding-left: 80px;
+`;
+
+export const ScrollContainer2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 120px;
 `;
 
 export const ClientLogo = styled.img`
-  width: 180px;
+  object-fit: contain;
 `;
