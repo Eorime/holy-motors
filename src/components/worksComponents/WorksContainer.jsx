3888cbd842143a9f7477573b6d4477fc8d8a01f1
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-06-27 15:22:16 +0400

    Add image style to the works page

--- src/components/worksComponents/WorksContainer.jsx
+++ src/components/worksComponents/WorksContainer.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import {
   Container,
+  ImageWrapper,
   WorkSliderContainer,
+  WorksImage,
+  WorksImageOverlay,
+  WorksName,
+  WorksNameContainer,
   WorksParagraph,
   WorksTextContainer,
 } from "./style";
+import { worksData } from "../../data/worksData";
 
 const WorksContainer = () => {
+  const work = worksData[0];
+
   return (
     <Container>
       <WorkSliderContainer>
+        <ImageWrapper>
+          <WorksImage src={work.image} />
+          <WorksImageOverlay />
+        </ImageWrapper>
+
+        <WorksNameContainer>
+          <WorksName>{work.name}</WorksName>
+        </WorksNameContainer>
         <WorksTextContainer>
           <WorksParagraph></WorksParagraph>
           <WorksParagraph></WorksParagraph>
--- src/components/worksComponents/style.js
+++ src/components/worksComponents/style.js
@@ -1,12 +1,35 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 100px 40px;
 `;
 
-export const WorkSliderContainer = styled.div``;
+export const WorkSliderContainer = styled.div`
+  max-width: 100%;
+  position: relative;
+`;
 
-export const WorksImage = styled.img``;
+export const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const WorksImage = styled.img`
+  max-width: 100%;
+`;
+
+export const WorksImageOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 99.6%;
+  background: linear-gradient(to top, rgba(0, 0, 0, 0.7) 2%, rgba(0, 0, 0, 0));
+  pointer-events: none;
+`;
 
 export const WorksTextContainer = styled.div`
   display: flex;
@@ -16,10 +39,25 @@ export const WorksTextContainer = styled.div`
 export const WorksParagraph = styled.p`
   color: #ffffff;
   font-size: 26px;
+  max-width: 800px;
+`;
+
+export const WorksNameContainer = styled.div`
+  background-color: #ff0000;
+  padding-top: 30px;
+  padding-left: 30px;
+  position: absolute;
+  display: inline-block;
+  bottom: 80px;
+  right: -1px;
 `;
 
 export const WorksName = styled.h1`
   font-family: "Mango", sans-serif;
   font-size: 110px;
   color: #ffffff;
+  max-width: 570px;
+  margin: 0;
+  padding: 0;
+  position: relative;
 `;
